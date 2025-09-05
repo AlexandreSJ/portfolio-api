f5f79028b149cc3cdd7d42d03435dd44a2a40cf4
AlexandreSJ <alexandre.sj22@gmail.com>
2025-09-04 21:51:50 -0300

    Revert "refactor:  Imports e construtor removidos"
    This reverts commit d716d4c1b723c22dc96a5535dd13b10ec15987a4.

--- src/main/java/br/com/aelxand/portfolioapi/PortfolioApiApplication.java
+++ src/main/java/br/com/aelxand/portfolioapi/PortfolioApiApplication.java
@@ -1,10 +1,14 @@
 package br.com.aelxand.portfolioapi;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class PortfolioApiApplication {
+
+    @Autowired
+    public PortfolioApiApplication(){}
 
 	public static void main(String[] args) {
 		SpringApplication.run(PortfolioApiApplication.class, args);
