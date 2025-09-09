6812f44339717f82860f0ccb72cc56ab0bdfc850
AlexandreSJ <alexandre.sj22@gmail.com>
2025-09-08 23:52:38 -0300

    refactor(imports): Removed unused imports

--- src/main/java/br/com/aelxand/portfolioapi/controllers/API.java
+++ src/main/java/br/com/aelxand/portfolioapi/controllers/API.java
@@ -1,9 +1,7 @@
 package br.com.aelxand.portfolioapi.controllers;
 
 import br.com.aelxand.portfolioapi.config.GeneralConfig;
-import br.com.aelxand.portfolioapi.controllers.interceptor.CustomRequestInterceptor;
 import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
